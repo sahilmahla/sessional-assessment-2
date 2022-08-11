ee20144f17479398c9481cdd4f25a850295b21b4
Sahil Mahla <sahilmahla1697@gmail.com>
2022-08-11 18:20:59 +0530

    improved person details card added some styles

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ function loadPerson(data){
 
         var personDiv = document.createElement('div');
         personDiv.setAttribute('id', 'person-card');
-        var centerboxDiv = document.createElement('div');
-        centerboxDiv.setAttribute('class', 'contact-box center-version');
+        // var centerboxDiv = document.createElement('div');
+        // centerboxDiv.setAttribute('class', 'contact-box center-version');
         // var anchortag = document.createElement('a');
         // anchortag.setAttribute('href', '#');
         // var personAvatar = document.createElement('img');
@@ -17,43 +17,57 @@ function loadPerson(data){
         // var name = document.createElement('h3');
         
         var personBody = `
-        <a href="#">
-            <img
-            alt="image"
-            class="img-circle"
-            src="https://bootdey.com/img/Content/avatar/avatar${randNum}.png"
-        />
-        <h3 class="m-b-xs"><strong>`+person.name+`</strong></h3>
-
-        <div class="font-bold">`+person.username+`</div>
-        <address class="m-t-md">
-            <strong>`+person.company.name+`</strong><br />
-            `+person.address.street+','+ person.suite+`<br />
-            `+person.address.city+' , '+person.address.zipcode+`<br />
-            <abbr title="Phone">P:</abbr>`+person.phone+`
-        </address>
-        </a>
-        <div class="contact-box-footer">
-        <div class="m-t-xs btn-group">
-          <a class="btn btn-xs btn-white"
-            ><i class="fa fa-phone"></i> Call
-          </a>
-          <a class="btn btn-xs btn-white"
-            ><i class="fa fa-envelope"></i> Email</a
-          >
-          <a class="btn btn-xs btn-white"
-            ><i class="fa-solid fa-globe"></i> Website</a
-          >
+        <article class="material-card Red">
+        <h2>
+            <span>${person.name}</span>
+            <strong>
+                <i class="fa fa-fw fa-star"></i>
+                @${person.username}
+            </strong>
+        </h2>
+        <div class="mc-content">
+            <div class="img-container">
+                <img
+                alt="image"
+                class="img-circle"
+                src="https://bootdey.com/img/Content/avatar/avatar${randNum}.png"
+              />
+          
+              <address class="m-t-md">
+                <strong>${person.email}</strong><br />
+                ${person.address.street}, ${person.address.suite}<br />
+                ${person.address.city}, ${person.address.zipcode}<br />
+                <abbr title="Phone">P:</abbr> ${person.phone}
+              </address>
+            </div>
+            <div class="mc-description">
+                <strong>Company Details</strong><br />
+                 Name: ${person.company.name}<br />
+                 Phrase: ${person.company.catchPhrase}<br />
+                BS: ${person.company.bs} <br />
+            </div>
         </div>
-      </div>
+        <a class="mc-btn-action">
+            <i class="fa fa-bars"></i>
+        </a>
+        <div class="mc-footer">
+            <h4>
+                Social
+            </h4>
+            <a class="fa fa-fw fa-phone"></a>
+            <a class="fa fa-fw fa-envelope"></a>
+            <a class="fa fa-fw fa-solid fa-globe"></a>
+            <a class="fa fa-fw fa-google-plus"></a>
+        </div>
+    </article>
         `;
 
-        var personFooter = `        `;
-        personDiv.appendChild(centerboxDiv);
+        // var personFooter = `        `;
+        // personDiv.appendChild(centerboxDiv);
      //   centerboxDiv.appendChild(anchortag);
         // centerboxDiv.appendChild(personFooter);
-        centerboxDiv.innerHTML = personBody;
-        document.getElementsByClassName('row')[0].appendChild(personDiv);
+        personDiv.innerHTML = personBody;
+        document.getElementsByClassName('row active-with-click')[0].appendChild(personDiv);
         
     }
 }
@@ -67,3 +81,34 @@ xhttp.onreadystatechange = function() {
 };
 xhttp.open("GET", apiUrl, true);
 xhttp.send();
+
+
+$(function() {
+    $('.material-card > .mc-btn-action').click(function () {
+        var card = $(this).parent('.material-card');
+        var icon = $(this).children('i');
+        icon.addClass('fa-spin-fast');
+
+        if (card.hasClass('mc-active')) {
+            card.removeClass('mc-active');
+
+            window.setTimeout(function() {
+                icon
+                    .removeClass('fa-arrow-left')
+                    .removeClass('fa-spin-fast')
+                    .addClass('fa-bars');
+
+            }, 800);
+        } else {
+            card.addClass('mc-active');
+
+            window.setTimeout(function() {
+                icon
+                    .removeClass('fa-bars')
+                    .removeClass('fa-spin-fast')
+                    .addClass('fa-arrow-left');
+
+            }, 800);
+        }
+    });
+});
